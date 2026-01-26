bbd4d57cee493668471268598b212f3d95cf7c6e
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-01-26 15:05:57 +0500

    Add method set() , get() and has()
    set takes a key if a key already exists inside a bucket updates its value otherwise it populates an index of the bucket based on the value returned by hash function
    (to populate bucket)
    
    get finds the value of a key if it exists
    
    has returns true or false based on whether a key exists or not

--- HashMap.js
+++ HashMap.js
@@ -2,6 +2,15 @@ class HashMap {
   constructor(loadFactor, capacity) {
     this.loadFactor = loadFactor;
     this.capacity = capacity;
+    this.bs = this.bucketSize(this.capacity);
+  }
+
+  bucketSize(capacity) {
+    let bs = [];
+    for (let i = 0; i < capacity; i++) {
+      bs.push([]);
+    }
+    return bs;
   }
 
   hash(key) {
@@ -12,9 +21,40 @@ class HashMap {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
       hashCode = hashCode % this.capacity;
     }
-
+    hashCode=hashCode % this.bs.length
     return hashCode;
   }
 
-  set(key, value) {}
+  set(key, value) {
+    let hashCode = this.hash(key);
+    for (element of this.bs) {
+      if (element.key == hashCode) {
+        element.value = value;
+        return;
+      }
+    }
+
+    this.bs[hashCode].push({ key, value });
+  }
+
+  get(key) {
+    let hashCode = this.hash(key);
+    for (element of this.bs) {
+      if (element.key == hashCode) return value;
+    }
+    return null;
+  }
+
+  has(key){
+     let hashCode = this.hash(key);
+    for (element of this.bs) {
+      if (element.key == hashCode) return true;
+    }
+    return false;
+  }
+
+  
 }
+
+let hashmap = new HashMap(0.75, 16);
+console.log(hashmap.bs);
