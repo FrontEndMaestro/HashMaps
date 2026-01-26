10c4593e249b1d24660577ac8672bbaade8cad27
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-01-27 01:21:49 +0500

    Add Linkedlist class and method to grow bucket
    Linkedlist class so that each bucket points to a linkedlist so we can
    store multiple nodes in case of collision
    
    growBucket method which multiplies capacity with load and if the total
    number of entries is equal to it then doubles the size of the bucket to
    keep collisions minimal
    
    Implement other methods:
    -add to linkedlist
    -get all entries in a linkedlist
    -print a linkedlist
    -display HashMap
    -get all entries in HashMap
    -clear everything in HashMap

--- HashMap.js
+++ HashMap.js
@@ -1,3 +1,5 @@
+import LinkedList from "./linked-list.js";
+
 class HashMap {
   constructor(loadFactor, capacity) {
     this.loadFactor = loadFactor;
@@ -8,9 +10,32 @@ class HashMap {
   bucketSize(capacity) {
     let bs = [];
     for (let i = 0; i < capacity; i++) {
-      bs.push([]);
+      let link = new LinkedList();
+      bs.push(link);
     }
     return bs;
+  }
+
+  growBucket(capacity, loadFactor) {
+    let copyArray;
+    let numOfEntries = Math.round(capacity * loadFactor);
+    if (numOfEntries == this.length()) {
+      copyArray = this.bs;
+      this.capacity = capacity * 2;
+      this.bs = this.bucketSize(this.capacity);
+      for (let i = 0; i < copyArray.length; i++) {
+        this.passKeysToHash(copyArray);
+        let linklist = copyArray[i];
+        this.bs[i] = copyArray[i];
+      }
+      //this.bs = this.bucketSize(capacity * 2);
+    }
+    //console.log(copyArray[1].toString());
+  }
+
+  passKeysToHash(copyArray) {
+    let allEntries = getAllEntries(copyArray);
+    console.log(allEntries);
   }
 
   hash(key) {
@@ -21,40 +46,95 @@ class HashMap {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
       hashCode = hashCode % this.capacity;
     }
-    hashCode=hashCode % this.bs.length
+    //hashCode = hashCode % this.bs.length;
     return hashCode;
   }
 
   set(key, value) {
-    let hashCode = this.hash(key);
-    for (element of this.bs) {
-      if (element.key == hashCode) {
-        element.value = value;
-        return;
-      }
+    let linkList = this.bs[this.hash(key)];
+    if (linkList.updateValue(key, value)) {
+    } else {
+      linkList.append(key, value);
     }
-
-    this.bs[hashCode].push({ key, value });
   }
 
   get(key) {
-    let hashCode = this.hash(key);
-    for (element of this.bs) {
-      if (element.key == hashCode) return value;
-    }
-    return null;
+    let linkList = this.bs[this.hash(key)];
+    return linkList.getValue(key);
   }
 
-  has(key){
-     let hashCode = this.hash(key);
-    for (element of this.bs) {
-      if (element.key == hashCode) return true;
-    }
-    return false;
+  has(key) {
+    let linkList = this.bs[this.hash(key)];
+    linkList.contains(key);
   }
 
-  
+  remove(key) {
+    let linkList = this.bs[this.hash(key)];
+    linkList.remove(key);
+  }
+
+  length() {
+    let count = 0;
+    for (let i = 0; i < this.bs.length; i++) {
+      let linkList = this.bs[i];
+      count += linkList.size();
+    }
+    return count;
+  }
+  clear() {
+    for (let i = 0; i < this.bs.length; i++) {
+      this.bs[i].removeAll();
+    }
+  }
+  keys() {
+    let keys = [];
+    for (let i = 0; i < this.bs.length; i++) {
+      keys.push(...this.bs[i].getAllKeys());
+    }
+    return keys;
+  }
+  values() {
+    let values = [];
+    for (let i = 0; i < this.bs.length; i++) {
+      values.push(...this.bs[i].getAllValues());
+    }
+    return values;
+  }
+
+  entries() {
+    let entries = [];
+    for (let i = 0; i < this.bs.length; i++) {
+      let linkedlist = this.bs[i].getAllEntries();
+      if (linkedlist.head != null) {
+        entries.push(...linkedlist);
+      }
+    }
+    return entries;
+  }
+
+  print() {
+    for (let i = 0; i < this.bs.length; i++) {
+      console.log(`[${this.bs[i].toString()}]\n`);
+    }
+  }
 }
 
-let hashmap = new HashMap(0.75, 16);
-console.log(hashmap.bs);
+let test = new HashMap(0.75, 16);
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
+test.print();
+//console.log(test.bs);
+
+console.log(test.length());
+test.growBucket(test.capacity, test.loadFactor);
+test.print();
---
+++ linked-list.js
@@ -0,0 +1,183 @@
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  append(key, value) {
+    let node = nodeClass();
+    node.setValue(value);
+    node.setKey(key);
+    if (this.head == null) {
+      this.head = node;
+      return;
+    }
+    let temp = this.head;
+    while (temp.nextNode != null) temp = temp.nextNode;
+    temp.nextNode = node;
+    node.nextNode = null;
+  }
+
+  printall() {
+    if (this.head == null) return "";
+    let temp = this.head;
+    while (temp != null) {
+      process.stdout.write(
+        `[key: ${temp.getkey()},value: ${temp.getValue()}]->`,
+      );
+      if (temp.nextNode == null) {
+        process.stdout.write("null");
+      }
+      temp = temp.nextNode;
+    }
+  }
+
+  size() {
+    let temp = this.head;
+    let count = 0;
+    while (temp != null) {
+      temp = temp.nextNode;
+      count++;
+    }
+    return count;
+  }
+
+  at(index) {
+    let count = 0;
+    let temp = this.head;
+    while (temp != null) {
+      if (count == index) return temp.getValue();
+      temp = temp.nextNode;
+      count++;
+    }
+    return undefined;
+  }
+
+  pop() {
+    if (this.head == null) return undefined;
+    let temp = this.head.nextNode;
+    let value = this.head.getValue();
+    this.head.nextNode = null;
+    this.head = temp;
+    return value;
+  }
+
+  updateValue(key, value) {
+    let temp = this.head;
+    while (temp != null) {
+      if (key == temp.getkey()) {
+        temp.setValue(value);
+        return true;
+      }
+      temp = temp.nextNode;
+    }
+    return false;
+  }
+
+  contains(key) {
+    let temp = this.head;
+    while (temp != null) {
+      if (key == temp.getkey()) {
+        return true;
+      }
+      temp = temp.nextNode;
+    }
+    return false;
+  }
+
+  remove(key) {
+    let temp = this.head;
+    let prev = null;
+    while (temp != null) {
+      if (key == temp.getkey()) {
+        prev.nextNode = temp.nextNode;
+        temp.nextNode = null;
+      }
+      prev = temp;
+      temp = temp.nextNode;
+    }
+    return false;
+  }
+
+  removeAll() {
+    this.head.nextNode = null;
+    this.head = null;
+  }
+
+  getValue(key) {
+    let temp = this.head;
+    while (temp != null) {
+      if (key == temp.getkey()) {
+        return temp.getValue();
+      }
+      temp = temp.nextNode;
+    }
+    return null;
+  }
+
+  getAllKeys() {
+    let keys = [];
+    let temp = this.head;
+    while (temp != null) {
+      keys.push(temp.getkey());
+      temp = temp.nextNode;
+    }
+    return keys;
+  }
+  getAllValues() {
+    let values = [];
+    let temp = this.head;
+    while (temp != null) {
+      keys.push(temp.getValue());
+      temp = temp.nextNode;
+    }
+    return values;
+  }
+
+  getAllEntries() {
+    let entries = [];
+    let temp = this.head;
+    while (temp != null) {
+      entries.push([temp.getkey(), temp.getValue()]);
+      temp = temp.nextNode;
+    }
+    return entries;
+  }
+
+  toString() {
+    if (this.head == null) return "";
+    let string = " ";
+    let temp = this.head;
+    while (temp != null) {
+      string += `[key: ${temp.getkey()},value: ${temp.getValue()}]->`;
+      if (temp.nextNode == null) {
+        string += `null`;
+        return string;
+      }
+      temp = temp.nextNode;
+    }
+  }
+
+  throwError(index) {
+    if (index < 0 || index > this.size()) throw new RangeError("Out of bounds");
+  }
+}
+
+function nodeClass() {
+  let value, nextNode, key;
+  value = nextNode = key = null;
+  function getValue() {
+    return value;
+  }
+
+  function getkey() {
+    return key;
+  }
+  function setKey(k) {
+    key = k;
+  }
+  const setValue = function (val) {
+    value = val;
+  };
+  return { nextNode, setKey, setValue, getkey, getValue };
+}
+
+export default LinkedList;
